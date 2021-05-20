fa20fb66d18562aab51b2bb2476e8c1756db1863
RG-Mac <ramong5406@gmail.com>
2021-05-20 17:10:05 -0500

    added question state and answer object to app.tsk

--- src/API.ts
+++ src/API.ts
@@ -1,9 +1,11 @@
+import { shuffleArray } from "./utils";
+
 
 export type Question = {
     category: string;
     correct_answer: string;
     difficulty: string;
-    incorrect_answer: string[];
+    incorrect_answers: string[];
     question: string;
     type: string;
 };
@@ -20,5 +22,13 @@ export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty)
     const endPoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
     const data = await (await fetch(endPoint)).json();
     console.log(data);
+    return data.results.map((question: Question) => (
+        {
+            ...question,
+            answers: shuffleArray([
+                ...question.incorrect_answers,
+                question.correct_answer,
+            ]),
+        }));
 };
  
